652bd01c9eb9c1fb4a247591eeeefcc076970371
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2017-11-14 09:30:24 +0000

    oops missed a comma

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     loaders: [
         { test: function (modulePath) {
                 return modulePath.endsWith('.ts') && !modulePath.endsWith('spec.ts');
-                }
+                },
           loader: 'ts-loader',
         }
     ]
